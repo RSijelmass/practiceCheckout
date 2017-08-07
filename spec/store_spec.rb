cfd5920293dba6060bfa992e0dbf60e899ab78a6
RSijelmass <R.Sijelmass@gmail.com>
2017-08-07 19:49:29 +0100

    Refactor tests Store  to include double of basket

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -1,7 +1,8 @@
 require 'store'
 
 describe Store do
-	let(:store) { Store.new() }
+	let(:basket) { double(:basket, input: {}, items: { tshirt: 1, voucher: 1, mug: 1} ) }
+	let(:store) { Store.new(basket) }
 
 	describe 'scans items' do
 		it 'scans a t-shirt' do
@@ -20,27 +21,24 @@ describe Store do
 
 	describe 'gives total price in basket' do
 		it 'gives 0 if basket is empty' do
-			expect(store.total_price).to eq 0
+			store2 = Store.new
+			expect(store2.total_price).to eq 0
 		end
-		it 'gives the price if one item has been scanned' do
-			store.scan(:voucher)
-			expect(store.total_price).to eq 500
-		end
-		it 'gives the total price if more than one item has been scanned' do
-			store.scan(:voucher)
-			store.scan(:mug)
-			expect(store.total_price).to eq 1250
+		it 'gives the price for all items in basket' do
+			expect(store.total_price).to eq 3250
 		end
 	end
 
 	describe 'handles special promotions' do
 		it 'gives a 2-for-1 offer on vouchers' do
-			6.times { store.scan(:voucher) }
-			expect(store.total_price).to eq 1500
+			basket_vouchers = double(:basket, items: { voucher: 6 } )
+			store3 = Store.new(basket_vouchers)
+			expect(store3.total_price).to eq 1500
 		end
 		it 'gives a discount on tshirts of €19 each if 3 or more bought' do
-			3.times { store.scan(:tshirt) }
-			expect(store.total_price).to eq 5700
+			basket_tshirts= double(:basket, items: { tshirt: 3 } )
+			store4 = Store.new(basket_tshirts)
+			expect(store4.total_price).to eq 5700
 		end
 	end
 end
